02473642339e9e37d3f5f333d7d2d8bcfb2786e3
Lasse Diercks <github@lassediercks.de>
2026-05-20 22:34:24 +0200

    feat: init

---
+++ Formula/flowgo.rb
@@ -0,0 +1,38 @@
+class Flowgo < Formula
+  desc "Browser-based mind-map editor backed by a plain-text file"
+  homepage "https://github.com/lassediercks/flowgo"
+  version "0.1.1"
+  license "AGPL-3.0-only"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-darwin-arm64"
+      sha256 "2a6eea8f5c9c7c39cd40bcb87cf61c69a86153f875759ace10d3e7c2b3d68898"
+    end
+    on_intel do
+      url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-darwin-amd64"
+      sha256 "48ccf784899971096a312257257941a5df64b3ea041b08999624fc4bc0136e03"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-linux-arm64"
+      sha256 "115dc72000e1aac074b0eb39b1f646072bf2c26761579f77ff7fa827658d4658"
+    end
+    on_intel do
+      url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-linux-amd64"
+      sha256 "c2555478216a1b3b70f9c892a2c872b0e73e9dc835af4986cc325b4963f0ba3c"
+    end
+  end
+
+  def install
+    # Release assets are raw binaries (not tarballs) named flowgo-v{version}-{os}-{arch}.
+    # Rename to "flowgo" so $HOMEBREW_PREFIX/bin gets a clean name.
+    bin.install Dir["flowgo-*"].first => "flowgo"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/flowgo version")
+  end
+end
