b41f2ab051a5ba96aa8675457c8d8ab9d97681e7
lassediercks <1765584+lassediercks@users.noreply.github.com>
2026-08-01 13:53:05 +0000

    chore: bump flowgo to 0.1.9

--- Formula/flowgo.rb
+++ Formula/flowgo.rb
@@ -1,28 +1,28 @@
 class Flowgo < Formula
   desc "Browser-based mind-map editor backed by a plain-text file"
   homepage "https://github.com/lassediercks/flowgo"
-  version "0.1.8"
+  version "0.1.9"
   license "AGPL-3.0-only"
 
   on_macos do
     on_arm do
       url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-darwin-arm64"
-      sha256 "7365c0684b7abea6b1cb73e9bae32c0565dd9a792aa98f5ce28bb3978bcf8e70"
+      sha256 "b3790290b9ca7971778ec00c64e02f4491703f4d0011f6897af330e439de0f1c"
     end
     on_intel do
       url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-darwin-amd64"
-      sha256 "5532fcf7a27c6ae262880ee53d54078a0a0847a138d5010be00b36e23f4f43d0"
+      sha256 "9810f58c73892ef8ebe3edef7e9e7c74502f7c4f6a1381346decc22fc08bab7f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-linux-arm64"
-      sha256 "db81b679d8b97edf9920e7bff39f37fc3662b780dfebed2dd227bb0adf9ca22f"
+      sha256 "73b5c40ce1209feba3ca6c97c15f8da1f847e67eb80c4b27e88fb40e442734bb"
     end
     on_intel do
       url "https://github.com/lassediercks/flowgo/releases/download/v#{version}/flowgo-v#{version}-linux-amd64"
-      sha256 "02c211a42a169c1c57e79fda8dc2f85f671a330610198446c853dde71ad8d6f3"
+      sha256 "2f3226291aa6269f86338117e19fb3f30685dcacabbb754c39b83bb340ae9e5c"
     end
   end
 
